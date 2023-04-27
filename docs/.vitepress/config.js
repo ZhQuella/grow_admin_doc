bef424771759aa224f5e51eaa95809ad57243aea
张鱼烧。 <827265905@qq.com>
2023-04-27 13:21:05 +0800

    fix(modify): "修复内容"

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vitepress';
+const path = require('path');
 
 export default defineConfig({
   title: "Grow Admin",
-  base: "./",
-  cleanUrls: 'with-subfolders'
+  base: '/docs/',
+  cleanUrls: 'with-subfolders'，
+  head: [
+    [
+      'link',{ rel: 'icon', href: '/favicon.ico' }
+    ]
+  ]
 });
